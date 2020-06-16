20b16fbc9c60b7c739562e7b275078436c041986
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2020-06-16 09:10:47 -0500

    Add project files.

---
+++ BlazorPeliculas/Client/Helpers/StringUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Client.Helpers
+{
+    public class StringUtilities
+    {
+        /// <summary>
+        /// Retorna texto en mayúscula
+        /// </summary>
+        /// <param name="value">String a convertir</param>
+        /// <returns></returns>
+        public static string CovertToUpper(string value) => value.ToUpper();
+    }
+}

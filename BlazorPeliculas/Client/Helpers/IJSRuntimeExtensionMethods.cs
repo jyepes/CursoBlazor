1eeccdbf7a22e50e1097ad3dbf0065ae617fed5e
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2020-06-26 08:09:44 -0500

    Ejecutar código de javascript desde C#

---
+++ BlazorPeliculas/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("console.log", message);
+            return await js.InvokeAsync<bool>("confirm", message);
+        }
+
+
+    }
+}

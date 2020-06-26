cf73556550d798987e2f6d566935bd51eb06c245
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2020-06-26 07:25:17 -0500

    Herencia: separar el código HTML del código de C#

---
+++ BlazorPeliculas/Client/Pages/Counter.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Client.Pages
+{
+    public class CounterBase: ComponentBase
+    {
+        [Inject] protected ServicioSingleton Singleton { get; set; }
+
+        [Inject] protected ServicioTransient Transient { get; set; }
+
+        protected int currentCount = 0;
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+            Singleton.Valor = currentCount;
+            Transient.Valor = currentCount;
+        }
+    }
+}

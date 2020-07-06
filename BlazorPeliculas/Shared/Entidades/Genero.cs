dcc7f2e2fc46d7d7887d22278ac4a5334cee2eb4
Julio Cesar Yepes Ruiz <julio.yepes@3ysolutions.com.co>
2020-07-06 08:22:20 -0500

    Formulario: Editar géneros

--- BlazorPeliculas/Shared/Entidades/Genero.cs
+++ BlazorPeliculas/Shared/Entidades/Genero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BlazorPeliculas.Shared.Entidades
@@ -7,6 +8,8 @@ namespace BlazorPeliculas.Shared.Entidades
     public class Genero
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage ="El campo '{0}' es requerido")]
         public string Nombre { get; set; }
     }
 }
